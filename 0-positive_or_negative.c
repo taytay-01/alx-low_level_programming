2fc2091c4d557c5033a6841d1ee52d62cdb8e4da
taytay fatima zahra <taytayfatimazahra@gmail.com>
2023-06-16 12:23:51 -0700

    positive or négative

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,31 +1,29 @@
-/*
- * File: 0-positive_or_negative.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/* more headers goes there */
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
+ * main - Entry point
  *
- * Return: Always 0.
-*/
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%d is negative\n", n);
+	}
 	else
+	{
 		printf("%d is zero\n", n);
-
+	}
 	return (0);
 }
