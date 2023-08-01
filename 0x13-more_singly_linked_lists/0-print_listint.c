e638d338071a20d4d872a7f360f43b9efd76e2e0
taytay01 <taytayfatimazahra@gmail.com>
2023-08-01 16:59:55 -0700

    task1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *node = h;
+	size_t cont = 0:
+
+	while (node)
+	{
+		printf("%i\n" , node->n);
+		cont++;
+		node = node->next;
+	}
+
+	return (cont);
+}
