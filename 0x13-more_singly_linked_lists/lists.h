6986abbe7c83bf5b44019d015cd903a8e587e37e
taytay01 <taytayfatimazahra@gmail.com>
2023-08-01 17:30:16 -0700

    task22

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,23 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-/*
- * File: lists.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
-
 #include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
 typedef struct listint_s
 {
 	int n;
